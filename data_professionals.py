aa51301910675dc9a09898ea8d3dafd9411b1d49
Olawalebunmi <141925174+Olawalebunmi@users.noreply.github.com>
2025-01-09 13:37:31 +0100

    changes

--- data_professionals.py
+++ data_professionals.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
 #create a bold header
-st.markdown("**#Data Analysis Report**", unsafe_allow_html=True)
+st.header("Data Analysis Report")
 
-st.markdown("**##Overview**" , unsafe_allow_html=True)
+# Report content(Justified text using CSS)
+st.write("""Overview
 
-st.write("""
+
          In this report, we analyze the data analyst survey to identify current roles, years of experience in data related fields, tools mostly used, challenges and otherrelevent variables which will be discussed.
          Sixty-nine (69) respondents filled the form, and their responses will be use for the analysis.
          An exploratory data analysis will be conducted, along with relevant visualizations to gain deep insights into the raw data.
