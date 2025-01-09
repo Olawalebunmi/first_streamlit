4209e10432a79ee8054a5d0a8996ae10d9dd4dd8
Olawalebunmi <141925174+Olawalebunmi@users.noreply.github.com>
2025-01-09 13:46:32 +0100

    update

--- data_professionals.py
+++ data_professionals.py
@@ -8,16 +8,16 @@ st.subheader("Overview")
 
 # write a sentence under a subheader
 st.write(""""
-         **In this report, we analyze the data analyst survey to identify current roles, years of experience in data related fields, tools mostly used, challenges and otherrelevent variables which will be discussed**.
+         In this report, we analyze the data analyst survey to identify current roles, years of experience in data related fields, tools mostly used, challenges and otherrelevent variables which will be discussed.
          Sixty-nine (69) respondents filled the form, and their responses will be use for the analysis.
          An exploratory data analysis will be conducted, along with relevant visualizations to gain deep insights into the raw data.
          The analysis will include findings on:
-         - Current role with the highest number of respondents.
-         - Current role and years spent in each field.
-         - The number of current roles in each field.
-         - Satisfaction level of people by their job roles.
-         - Tools mostly use by professionals for analysis.
-         - Industries that offer more job opportunities. 
+- Current role with the highest number of respondents.
+- Current role and years spent in each field.
+- The number of current roles in each field.
+- Satisfaction level of people by their job roles.
+- Tools mostly use by professionals for analysis.
+- Industries that offer more job opportunities. 
 
          """)
 
