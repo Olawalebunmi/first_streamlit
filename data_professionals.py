a99eda762d6c8fad0e270d2805a852bb8a879067
Olawalebunmi <141925174+Olawalebunmi@users.noreply.github.com>
2025-01-13 15:28:47 +0100

    update

--- data_professionals.py
+++ data_professionals.py
@@ -39,8 +39,42 @@ with col4:
                  title = "Role and Field of Analyst", hover_data = ["Field"],
                  template= "gridoff",height=500)
     st.plotly_chart(fig,use_container_width=True)
+
+#To view the data in the chart
+_, view1, dwn1, view2, dwn2 = st.columns([0.15,0.20,0.20,0.20,0.20])
+with view1:
+    expander = st.expander("Roles and Fields")
+    data = datahub[["Role", "Field"]].groupby(by="Role")["Field"].sum()
+    expander.write(data)
+
+#downloading the data in form of csv
+with dwn1:
+    st.download_button("Get Data", data = data.to_csv().encode("utf-8"),
+                       file_name="Role and Field.csv", mime="text/csv")
+
+#Creating another chart
+datahub["Month_Year"] = datahub["date"].dt.strftime("%b'%y")
+result = datahub.groupby(by=datahub["date"])["Field"].sum().reset_index()
+
+#with col5:
+    #fig1 = px.line(result, x= "Month_Year", y = "Field", title = "Total Fields Over Time",
+                   #template ="gridoff")
+    #st.plotly_chart(fig1,use_container_width=True)
+
+#with view2:
+#expander = st.expander("Fields over Time or Monthly Sales")
+    #data = result
+    #expander.write(data)
+
+#downloading the data in form of csv
+#with dwn2:
+    #st.download_button("Get Data", data = data.to_csv().encode("utf-8"),
+                       #file_name="Total Field Over Time.csv", mime="text/csv")
     
 
+
+#To add a straight line beloe chart
+st.divider()
 
 
 
