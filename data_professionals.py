9a7f4423c74360fbd71a5ca2c2a2d7c5b38f75e4
Olawalebunmi <141925174+Olawalebunmi@users.noreply.github.com>
2025-01-14 13:13:42 +0100

    update

--- data_professionals.py
+++ data_professionals.py
@@ -7,10 +7,10 @@ import plotly.graph_objects as go
 
 
 # Reading in data
-datahub = pd.read_csv(r"C:\Users\USER\Downloads\datahub.csv")
+datahub = pd.read_csv("newbies.csv")
 st.set_page_config("layout = wide")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
-image = Image.open(r'C:\Users\USER\Downloads\Data Analyst.png')
+image = Image.open('Data Analyst.png')
 
 col1, col2 = st.columns([0.1, 0.9])
 with col1:
