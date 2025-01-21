54a8278b6acbc3924c168fd29164c1d9c7e4fc0d
Iremiren Daniel Oluwasegun <142715968+doluwasegun@users.noreply.github.com>
2025-01-21 14:19:09 +0100

    change

--- data_professionals.py
+++ data_professionals.py
@@ -11,7 +11,7 @@ import plotly.graph_objects as go
 datahub = "newbies.csv"
 st.set_page_config("layout = wide")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
-image = Image.open('"C:\Users\USER\Downloads\Data Analyst.png"')
+#image = Image.open "Data Analyst.png"
 
 col1, col2 = st.columns([0.1, 0.9])
 with col1:
