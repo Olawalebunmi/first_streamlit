4303f41cbeec26eeb73554c82b54041eb5bd1a92
Iremiren Daniel Oluwasegun <142715968+doluwasegun@users.noreply.github.com>
2025-01-21 14:22:59 +0100

    push

--- data_professionals.py
+++ data_professionals.py
@@ -13,9 +13,8 @@ st.set_page_config("layout = wide")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 #image = Image.open "Data Analyst.png"
 
-#col1, col2 = st.columns([0.1, 0.9])
-#with col1:
-    #st.image(image, width=100)
+col2 = st.columns([0.9])
+
 
 html_title = """
 <style>
