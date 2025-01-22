4a95cf4b267d660ccdbf391ae45a7378f9f65e92
Olawalebunmi <141925174+Olawalebunmi@users.noreply.github.com>
2025-01-22 16:21:31 +0100

    Update untitled-1.py

--- untitled-1.py
+++ untitled-1.py
@@ -6,7 +6,7 @@ import os
 import warnings
 warnings.filterwarnings('ignore')
 
-st.write("Hello")
+st.write("Hello, how are you?")
 
 
 st.set_page_config(page_title="Datahub Newbies Survey", page_icon=":bar_chart:", layout="wide")
