bd5cd2ff898791f47eb92c2af4617388985fa436
Olawalebunmi <141925174+Olawalebunmi@users.noreply.github.com>
2025-01-23 21:28:36 +0100

    go

--- files.py
+++ files.py
@@ -8,7 +8,7 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-st.write("Hello")
+st.write("Hello there")
 # Page configuration
 st.set_page_config(
     page_title="Datahub Newbies Survey",
